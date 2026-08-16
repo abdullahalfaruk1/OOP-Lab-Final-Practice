76fa22adf038229f60cbb446a93a93cfea9973ba
Abdullah Al Faruk <alfaruk091@gmail.com>
2026-08-16 20:20:22 +0600

    added an abstract file & interface file

---
+++ InterfaceAbstractTogether/InterfaceAbstract.java
@@ -0,0 +1,14 @@
+package InterfaceAbstractTogether;
+interface A{
+    void show();
+}
+abstract class B{
+    abstract void display();
+
+    B(){
+        System.out.println("B constructor");
+    }
+}
+public class InterfaceAbstract {
+
+}
