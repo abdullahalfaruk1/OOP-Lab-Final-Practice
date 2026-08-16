addaf505f496c7eb89d9aa7ce062a3d1f60a06ac
Abdullah Al Faruk <alfaruk091@gmail.com>
2026-08-16 18:46:13 +0600

    added a main file

--- Interface/Interface.java
+++ Interface/Interface.java
@@ -9,15 +9,20 @@ interface B {
 }
 
 class C implements A, B {
+    public void displayA() {
+        System.out.println("Hello from A");
+    }
+
     public void displayB() {
         System.out.println("Hello from B");
     }
 
-    public void displayA() {
-        System.out.println("Hello from A");
-    }
 }
 
 public class Interface {
-
+    public static void main(String[] args) {
+        C obj = new C();
+        obj.displayA();
+        obj.displayB();
+    }
 }
